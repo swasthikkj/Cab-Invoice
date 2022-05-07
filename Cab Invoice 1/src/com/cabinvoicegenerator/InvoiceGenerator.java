d27cdecb57204e38693acdc8927452f983be6144
swasthikkj <99305988+swasthikkj@users.noreply.github.com>
2022-05-07 10:38:03 +0530

    program

---
+++ Cab Invoice 1/src/com/cabinvoicegenerator/InvoiceGenerator.java
@@ -0,0 +1,12 @@
+package com.cabinvoicegenerator;
+public class InvoiceGenerator {
+
+	private static final int COST_PER_TIME = 1;
+	private static final double COST_PER_KM = 10;
+	private static final double MINIMUM_FARE = 5;
+
+	public double calculateFare(double distance, int time) {
+		return Math.max(MINIMUM_FARE, distance * COST_PER_KM + time * COST_PER_TIME);
+	}
+
+}
---
+++ Cab Invoice 1/src/com/cabinvoicegenerator/InvoiceGenretorTest.java
@@ -0,0 +1,26 @@
+package com.cabinvoicegenerator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class InvoiceGenretorTest {
+
+	@Test
+	public void givenDistanceAndTime_ShouldReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 1.0;
+		int time = 10;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		assertEquals(20, fare, 0.0);
+	}
+
+	@Test
+	public void givenDistanceAndTime_WhenTotalFareLessThan10_ShouldReturnMinimumFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		assertEquals(5, fare, 0.0);
+	}
+}
